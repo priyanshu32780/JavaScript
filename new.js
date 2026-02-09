710b1eca74b6e53ca1e682364547ec7a178400d4
priyanshu jha <jhapriyanshu546@gmail.com>
2026-02-09 07:47:06 -0800

    Add files via upload

---
+++ new.js
@@ -0,0 +1,3 @@
+console.log("Hellow world")
+  console.log("Code is also running")
+  console.log("Code is just looking like a wow")
---
+++ script.js
@@ -0,0 +1,9 @@
+ 
+       alert ( "Hello world");
+  console.log("Code is running")
+  console.log("Code is also running")
+  console.log("Code is just looking like a wow")
+
+  var a = prompt("Enter your number")
+
+  console.log("Your number is" + a)
