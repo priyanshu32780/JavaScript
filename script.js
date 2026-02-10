f9dac940b67f7668f734d19fbc6efd929a03c6a3
priyanshu jha <jhapriyanshu546@gmail.com>
2026-02-10 08:25:28 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
- 
-       alert ( "Hello world");
-  console.log("Code is running")
-  console.log("Code is also running")
-  console.log("Code is just looking like a wow")
+console.log("Hey this a tutorial 2 of java script")
 
-  var a = prompt("Enter your number")
+var a = 5;
+// a = a+1
+let b = 6;
+let c = "Priyanshu jha";
+let _a = "Shubham";
+// var 55a = "Rohan"; Not allowed
+// console.log(a+b+8)
+// console.log(typeof a, typeof b, typeof c,)
 
-  console.log("Your number is" + a)
+{
+    // let a =66;
+    console.log(a)
+}
+console.log(a)
+
+
+// cont a1 = 6;
+// a1 = a1 + 1; // Not Allowed becouse a1 is contant
+
+
+let x = "Priynashu jha ";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log( x,y,z,p,q,r)
+
+console.log( typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+
+
+
+let o = {
+    "name": "Priyanshu",
+    "job code": 5600,
+    " is handsome":true
+}
+console.log(o);
+o.salary = "100crores";
+console.log(o);
+o.salary = "500crores";
+console.log(o)
