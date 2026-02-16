3551900b9df6d833cabab46e9a0f68d18f8012b0
priyanshu jha <jhapriyanshu546@gmail.com>
2026-02-16 06:07:58 -0800

    Add files via upload

---
+++ video58/index.js
@@ -0,0 +1,17 @@
+/*Create a faulty calculator using javascript
+
+This faulty calculater does following:
+1. It takes two number as input from the user
+2. It perfoms wrong operations as following:
+
++ ----> -
+* ----> +
+- ----> /
+/ ---> **
+
+It performs wrong opreation 10% of the time
+
+
+
+
+*/
