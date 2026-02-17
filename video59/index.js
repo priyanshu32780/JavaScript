40a85253b61c83df7af15751b4462c809251c338
priyanshu jha <jhapriyanshu546@gmail.com>
2026-02-17 07:33:52 -0800

    Add files via upload

---
+++ video59/index.js
@@ -0,0 +1,18 @@
+let arr = [ 1,2,4,5,7]
+//index =   0,1,2,3,4
+
+
+
+
+arr[0] = 5666;
+
+// console.log(arr, typeof arr)
+
+// console.log(arr.length)
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+// console.log(arr[4])
+
+console.log(arr.toString())
+console.log (arr.join(" and "))
