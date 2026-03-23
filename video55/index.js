ad21ccf316f86a2c79efe68fb29dff5064d4a8eb
priyanshu jha <jhapriyanshu546@gmail.com>
2026-03-23 21:34:31 +0530

    Add files via upload

---
+++ video55/index.js
@@ -0,0 +1,29 @@
+function nice(name) {
+    console.log("Hey" + name + " you are nice")
+    console.log("Hey" + name + " You are good")
+    console.log("Hey" + name + " your  tshirt is nice")
+    console.log("Hey" + name + " your course are good")
+}
+nice("Priyanshu")
+
+function sum(a,b,c=3) {
+    // console.log(a+b)
+    return a + b + c
+}
+result = sum(3,5)
+console.log("The sum of these number is: ", result)
+
+
+
+const func1 = (x)=>{
+    console.log("I am an arrow function",x)
+}
+
+func1(34);
+func1(66);
+func1(84);
+
+    
+
+
+
